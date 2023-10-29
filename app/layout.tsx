95b0571b18e62779c9c028a172b0a1b5abd0b09a
Caleb Fisher <cfishe36@vols.utk.edu>
2023-10-29 00:32:58 -0400

    makes metadata relevant

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,23 +15,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>LangChain + Next.js Template</title>
+        <title>Medflow</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Providing deeper insights for nurses and the relationships that they hold with patients."
         />
-        <meta property="og:title" content="LangChain + Next.js Template" />
+        <meta property="og:title" content="Medflow" />
         <meta
           property="og:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Providing deeper insights for nurses and the relationships that they hold with patients."
         />
         <meta property="og:image" content="/images/og-image.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
+        <meta name="twitter:title" content="Medflow" />
         <meta
           name="twitter:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Providing deeper insights for nurses and the relationships that they hold with patients."
         />
         <meta name="twitter:image" content="/images/og-image.png" />
       </head>
