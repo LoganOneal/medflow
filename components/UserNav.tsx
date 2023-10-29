168979be6173cc8798a0c268528ff0810a542691
Caleb Fisher <cfishe36@vols.utk.edu>
2023-10-29 12:15:03 -0400

    fixing avatar

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -22,63 +22,68 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 export default function UserNav() {
     const { user, error, isLoading } = useUser();
 
-    return (
-      user && 
-      (<DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src={user.picture!} alt={user.nickname!} />
-              <AvatarFallback>SC</AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{user.name}</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {user.email}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Profile
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Settings
-              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <a href="/api/auth/logout">
-            <DropdownMenuItem>
-              Logout
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </a>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      ) || (<DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarFallback></AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <a href="/api/auth/login">
-            <DropdownMenuItem>
-              Login
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </a>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    if (user) {
+      return (
+        user &&
+         (<DropdownMenu>
+           <DropdownMenuTrigger asChild>
+             <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+               <Avatar className="h-8 w-8">
+                 <AvatarImage src={user.picture!} alt={user.nickname!} />
+                 <AvatarFallback>SC</AvatarFallback>
+               </Avatar>
+             </Button>
+           </DropdownMenuTrigger>
+           <DropdownMenuContent className="w-56" align="end" forceMount>
+             <DropdownMenuLabel className="font-normal">
+               <div className="flex flex-col space-y-1">
+                 <p className="text-sm font-medium leading-none">{user.name}</p>
+                 <p className="text-xs leading-none text-muted-foreground">
+                   {user.email}
+                 </p>
+               </div>
+             </DropdownMenuLabel>
+             <DropdownMenuSeparator />
+             <DropdownMenuGroup>
+               <DropdownMenuItem>
+                 Profile
+                 <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+               </DropdownMenuItem>
+               <DropdownMenuItem>
+                 Settings
+                 <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+               </DropdownMenuItem>
+             </DropdownMenuGroup>
+             <DropdownMenuSeparator />
+             <a href="/api/auth/logout">
+               <DropdownMenuItem>
+                 Logout
+                 <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+               </DropdownMenuItem>
+             </a>
+           </DropdownMenuContent>
+         </DropdownMenu>
+         )
       )
-    )
+    } else {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+              <Avatar className="h-8 w-8 bg-white">
+                <AvatarFallback>TS</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <a href="/api/auth/login">
+              <DropdownMenuItem>
+                Login
+                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </a>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        )
+    }
   }
